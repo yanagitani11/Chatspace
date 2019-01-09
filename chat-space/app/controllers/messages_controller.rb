647943e087a8bfd0ca4161d988604532bb0e3cb8
yanagitani11 <liuguliangtai@yanagiyaryoufutoshinoMacBook-puro.local>
2019-01-10 01:49:04 +0900

    Edit controller

--- chat-space/app/controllers/messages_controller.rb
+++ chat-space/app/controllers/messages_controller.rb
@@ -1,6 +1,32 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
-	def index
-	end
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+    @group_user = @group_users.map(&:name)
+  end
+
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.html
+        format.json
+      end
+    else @message.nill?
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+    @group_users = @group.users
+  end
 
 end
