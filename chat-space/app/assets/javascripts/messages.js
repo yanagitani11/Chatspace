2ffcd432b4f0627611b8e8015d9ea69a2029ab39
yanagitani11 <liuguliangtai@yanagiyaryoufutoshinoMacBook-puro.local>
2019-01-10 01:46:58 +0900

    Create function buildHTML

--- chat-space/app/assets/javascripts/messages.js
+++ chat-space/app/assets/javascripts/messages.js
@@ -1,3 +1,21 @@
 $(function() {
 	$('.messages').scrollTop($('.messages')[0].scrollHeight);
 });
+	function buildHTML(message){
+    var image = message.image ? `<img src="${message.image}" class="chat-main__message-body"> ` : "" ;
+    var html = `<div class="message">
+    			  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">${message.body}</p>
+                    ${image}
+                  </div>
+                </div>`
+	return html;
+	}
