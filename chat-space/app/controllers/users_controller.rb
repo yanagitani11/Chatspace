e144fbe0c0d06d53b3701e45c569abe68ace78f9
yanagitani11 <liuguliangtai@yanagiyaryoufutoshinoMacBook-puro.local>
2019-01-15 16:34:23 +0900

    Edit users_controller

--- chat-space/app/controllers/users_controller.rb
+++ chat-space/app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%").where.not(name: current_user.name,id: current_user.id)
+    @users = User.where("name LIKE ? AND name NOT LIKE ? ", "%#{params[:keyword]}%","%#{current_user.name}%")
     respond_to do |format|
       format.html
       format.json
