dd5e1899f931d96d7c4e653f9a7a0cc51f8b3386
yanagitani11 <liuguliangtai@yanagiyaryoufutoshinoMacBook-puro.local>
2019-01-19 08:31:57 +0900

    Edit

---
+++ config 8.31.36/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "groups#index"
+  resources :users, only: [:edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+end
